87b75ba60ef148a5f1526de4f2772be63e95070a
lgerim <77569780+lgerim@users.noreply.github.com>
2021-02-02 22:32:43 +0100

    Add files via upload

---
+++ c05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgerim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/26 13:09:50 by lgerim            #+#    #+#             */
+/*   Updated: 2021/02/01 15:45:52 by lgerim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_iterative_factorial(int nb)
+{
+	int i;
+	int resultat;
+
+	i = 1;
+	resultat = 1;
+	if (nb < 0)
+	{
+		return (0);
+	}
+	else if (nb == 0)
+	{
+		return (1);
+	}
+	while (i <= nb)
+	{
+		resultat = resultat * i;
+		i++;
+	}
+	return (resultat);
+}

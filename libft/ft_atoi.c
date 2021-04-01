adf96d19659f67b9a371ab28d2a288b8f918fd9d
lgerim <77569780+lgerim@users.noreply.github.com>
2021-04-01 04:01:27 +0200

    Update ft_atoi.c

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -12,28 +12,31 @@
 
 #include "libft.h"
 
-int	ft_atoi(const char *str)
+int	ft_atoi(const char *str
 {
-	long	i;
-	long	nbr;
-	int		isneg;
+	int		i;
+	int		nbr;
+	int		nega;
 
 	i = 0;
-	nbr = 0;
-	isneg = 0;
-	while (str[i] != '\0' && (str[i] == 32 || str[i] == '\t' || str[i] == '\n'
-			|| str[i] == '\r' || str[i] == '\v' || str[i] == '\f'))
+
+	while ((str[i] == '\t') || (str[i] == '\n') || (str[i] == '\r')
+		|| (str[i] == '\v') || (str[i] == ' ') || (str[i] == '\f'))
 		i++;
-	if (str[i] != '\0' && str[i] == '-')
+	nega = 1;
+	if (str[i] == '+' || str[i] == '-')
 	{
-		isneg = 1;
+		if ((str[i + 1] == '-') || (str[i + 1] == '+'))
+			return (0);
+		if (str[i] == '-')
+			nega = -1;
 		i++;
 	}
-	else if (str[i] == '+')
+	nbr = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nbr = (nbr * 10) + (str[i] - 48);
 		i++;
-	while (str[i] != '\0' && ft_isdigit(str[i]))
-		nbr = (nbr * 10) + (str[i++] - '0');
-	if (isneg == 1)
-		return (-nbr);
-	return (nbr);
+	}
+	return (nbr * nega);
 }

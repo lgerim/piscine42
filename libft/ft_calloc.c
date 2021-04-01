eb2e39c377b018a1b616936855a21b37b3705ca2
lgerim <77569780+lgerim@users.noreply.github.com>
2021-04-01 04:06:57 +0200

    Update ft_calloc.c

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -12,15 +12,21 @@
 
 #include "libft.h"
 
-void
-	*ft_calloc(size_t count, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
-	void	*ptr;
+	char	*str;
+	size_t	i;
 
-	ptr = (void*)malloc(count * size);
-	if (!ptr)
+	str = (char *)malloc(size * count);
+	if (!str)
 		return (NULL);
-	ft_bzero(ptr, count);
-	return (ptr);
+	i = 0;
+	while (i != size * count)
+	{
+		str[i] = 0;
+		i++;
+	}	
+	return ((void *)str);
 }
+
 
